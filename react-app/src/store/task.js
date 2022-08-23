57e2b1d3e88fd2aedfdabf541f85fc209920d7da
Steve Choi <stevech071@gmail.com>
2022-08-23 12:40:40 -0700

    task Create / Store & backend route connected // Issue with error 500 - not resolved yet

--- react-app/src/store/task.js
+++ react-app/src/store/task.js
@@ -1,22 +1,49 @@
 // constants
 const GET_TASKS = 'task/GET_TASKS';
+const ADD_TASK = 'task/ADD_TASK';
 
 const getTasks = (data) => ({
     type: GET_TASKS,
     payload: data
 });
 
+const addATask = (data) => ({
+    type: ADD_TASK,
+    payload: data
+});
+
 export const dataCallTasks = () => async (dispatch) => {
-    //Fetch call
     const response = await fetch('/api/tasks', {
         headers: {
             'Content-Type': 'application/json'
         }
     })
-    //Checking for errors -> 
     if (response.ok) {
         const data = await response.json();
         dispatch(getTasks(data))
+        return null;
+    } else if (response.status < 500) {
+        const data = await response.json();
+        if (data.errors) {
+            return data.errors;
+        }
+    } else {
+        return ['An error occurred. Please try again.']
+    }
+}
+
+export const addTask = (data) => async (dispatch) => {
+    const formData = new FormData();
+    const response = await fetch('/api/tasks/new', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData),
+    })
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(addATask(data))
         return null;
     } else if (response.status < 500) {
         const data = await response.json();
@@ -36,6 +63,11 @@ export default function reducer(state = initialState, action) {
             const newState = {};
             action.payload.tasks.forEach(task => newState[task.id] = task)
             return newState;
+        case ADD_TASK: {
+                const newState = { ...state };
+                newState[action.payload.id] = action.payload;
+                return newState;
+              }
         default:
             return state;
     }
