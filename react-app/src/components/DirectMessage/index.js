3081c7a453ed597caac2cdbb99f3e812b0ee3255
Jared Kunhart <hakujaden@comcast.net>
2022-09-04 21:42:00 -0500

    styling for direct messages

--- react-app/src/components/DirectMessage/index.js
+++ react-app/src/components/DirectMessage/index.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { io } from 'socket.io-client';
+import './style.css';
+
+
 let socket;
 
 const DirectMessage = () => {
@@ -26,24 +29,45 @@ const DirectMessage = () => {
 
     const sendChat = (e) => {
         e.preventDefault()
-        socket.emit("dm", { user: user.username, content: chatInput });
+        socket.emit("dm", { user: user.username, content: chatInput }); // id: user.id
         setChatInput("")
     }
 
     return (user && (
-        <div>
-            <div>
+        <div id="container">
+            <div id="dm-header">
+                <div id="dm-grp-img">
+                    <img class="i1" src="https://images.unsplash.com/photo-1660476705851-21e527337f9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+                    <img class="i2" src="https://images.unsplash.com/photo-1657299143482-4f4ea1ebd71c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+                    <img class="i3" src="https://images.unsplash.com/photo-1660481451479-7ad6d6ad0223?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                </div>
+                <div id="dm-grp-info">
+                    <h3 class="dm-grp-name">
+                        Steve, Damian, Jared
+                    </h3>
+                </div>
+            </div>
+            <div id="dm-chatbox">
                 {messages.map((message, ind) => (
-                    <div key={ind}>{`${message.user}: ${message.content}`}</div>
+                    <div id="dm-chat">
+                        <p id="dm-msg">
+                            <div key={ind}>{`${message.user}: ${message.content}`}</div>
+                        </p>
+                    </div>
                 ))}
             </div>
-            <form onSubmit={sendChat}>
-                <input
-                    value={chatInput}
-                    onChange={updateChatInput}
-                />
-                <button type="submit">Send</button>
-            </form>
+
+            <div id="dm-chat-form">
+                <form id="dm-form" onSubmit={sendChat}>
+                    <input
+                        id="dm-chat-input"
+                        value={chatInput}
+                        onChange={updateChatInput}
+                    />
+                    <div></div>
+                    <button id="dm-submit" type="submit">Send</button>
+                </form>
+            </div>
         </div>
     )
     )
